43f889d660e6ae94836ec504c83741af835029fe
dasWarder <Al.94_94@mail.ru>
2021-08-26 14:51:19 +0300

    Add scheduler for heroku

--- src/main/java/by/itechart/weather_bot/WeatherBotApplication.java
+++ src/main/java/by/itechart/weather_bot/WeatherBotApplication.java
@@ -2,8 +2,10 @@ package by.itechart.weather_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+@EnableScheduling
 @SpringBootApplication
 public class WeatherBotApplication {
 
