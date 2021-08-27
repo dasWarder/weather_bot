4084e0f20563ea8d1c0e1b501c640c1d80560656
dasWarder <Al.94_94@mail.ru>
2021-08-27 13:44:16 +0300

    Add scheduling

---
+++ src/main/java/by/itechart/weather_bot/schedule/HerokuScheduling.java
@@ -0,0 +1,29 @@
+package by.itechart.weather_bot.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Slf4j
+@Component
+public class HerokuScheduling {
+
+    private static final String REQUEST_URL = "https://google.com";
+
+    @Scheduled(cron = "* 15 * * * * *")
+    public void ping() {
+        try {
+            URL url = new URL(REQUEST_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Ping connection to the {}", REQUEST_URL);
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
